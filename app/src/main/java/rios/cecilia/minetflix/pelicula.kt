37b5327ff32832d356f8cc411a80cc3736048793
CMRR1 <38302840+CMRR1@users.noreply.github.com>
2020-03-02 21:29:58 -0700

    Practica Netflix

---
+++ app/src/main/java/rios/cecilia/minetflix/pelicula.kt
@@ -0,0 +1,6 @@
+package rios.cecilia.minetflix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+data class Pelicula(var titulo: String,var image: Int, var header: Int, var sinopsis: String)
